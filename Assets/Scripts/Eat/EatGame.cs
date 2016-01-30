6bed05ba5abe914636955e23bece11fa6000c102
Christopher Foster <fosterchris@comcast.net>
2016-01-30 09:50:09 -0500

    body and head animations for eat

--- Assets/Scripts/Eat/EatGame.cs
+++ Assets/Scripts/Eat/EatGame.cs
@@ -7,6 +7,8 @@ public class EatGame : MonoBehaviour {
     public bool isActing = false;
     public Animator anchorAnim;
     public Animator handAnim;
+    public Animator headAnim;
+    public Animator bodyAnim;
     public float maxAnchorY = 0;
     public float minAnchorY = 0;
     MiniGame miniGame;
@@ -31,19 +33,19 @@ public class EatGame : MonoBehaviour {
 	}
 
     // woo doesn't work
-    void ResetAction()
-    {
-        Debug.Log("Woo, lets reset!");
-        handAnim.ResetTrigger("result_too_low");
-        handAnim.ResetTrigger("result_too_high");
-        handAnim.ResetTrigger("result_just_right");
-        anchorAnim.ResetTrigger("result_just_right");
-        anchorAnim.StopPlayback();
-        anchorAnim.Play("hand_updown");
-        anchorAnim.StartPlayback();
-        handAnim.Play("hand_reset");
-        hasActed = false;
-    }
+//    void ResetAction()
+//    {
+//        Debug.Log("Woo, lets reset!");
+//        handAnim.ResetTrigger("result_too_low");
+//        handAnim.ResetTrigger("result_too_high");
+//        handAnim.ResetTrigger("result_just_right");
+//        anchorAnim.ResetTrigger("result_just_right");
+//        anchorAnim.StopPlayback();
+//        anchorAnim.Play("hand_updown");
+//        anchorAnim.StartPlayback();
+//        handAnim.Play("hand_reset");
+//        hasActed = false;
+//    }
 
     void ResolveAction()
     {
@@ -53,18 +55,24 @@ public class EatGame : MonoBehaviour {
         {
             handAnim.SetTrigger("result_too_low");
             anchorAnim.Stop();
+            headAnim.SetTrigger("result_fail_01");
+            bodyAnim.SetTrigger("result_fail_01");
             miniGame.ReportLose();
         }
         else if (anchor_y > maxAnchorY)
         {
             handAnim.SetTrigger("result_too_high");
             anchorAnim.Stop();
+            headAnim.SetTrigger("result_fail_01");
+            bodyAnim.SetTrigger("result_fail_01");
             miniGame.ReportLose();
         }
         else
         {
             handAnim.SetTrigger("result_just_right");
             anchorAnim.SetTrigger("result_just_right");
+            headAnim.SetTrigger("result_success_01");
+            bodyAnim.SetTrigger("result_success_01");
             miniGame.ReportWin();
         }
         hasActed = true;
