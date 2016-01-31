043f1fffa9fdf8df574af0fe4171fa6288bafc8a
Kevin Cavanaugh <kevrcav@gmail.com>
2016-01-30 20:37:21 -0500

    Added a gradient effect to the happiness and timer bar

---
+++ Assets/Scripts/SliderGradientController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SliderGradientController : MonoBehaviour {
+
+   Image fill;
+   public Gradient gradient;
+
+   void Init()
+   {
+      fill = GetComponent<Slider>().fillRect.GetComponent<Image>();
+   }
+
+   public void UpdateColor(float percent)
+   {
+      if (fill == null)
+         Init();
+      fill.color = gradient.Evaluate(percent);
+   }
+}
